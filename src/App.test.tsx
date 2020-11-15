ed53a2b5ecee8371f7421122bd25b505958bdc18
JohnIrle <irlejohn@gmail.com>
2020-11-15 17:59:57 -0600

    Add Tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,25 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
-it("renders correctly", () => {
+afterEach(() => cleanup());
+
+test("renders", async () => {
   render(<App />);
+});
+
+test("displays inital example", async () => {
+  render(<App />);
+  expect(screen.getByText("# Try me ## I work")).toBeInTheDocument();
+  expect(screen.getByText("Try me")).toBeInTheDocument();
+  expect(screen.getByText("I work")).toBeInTheDocument();
+});
+
+test("renders new markdown", async () => {
+  render(<App />);
+  userEvent.type(screen.getByRole("textbox"), "{enter} # A new header");
+  userEvent.type(screen.getByRole("textbox"), "{enter} ## Another h2");
+  expect(screen.getByText("A new header")).toBeInTheDocument();
+  expect(screen.getByText("Another h2")).toBeInTheDocument();
 });
